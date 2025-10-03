d6844509a9bcaafe9ba97634dd55c2044aa8abdd
kanatdavid <david.kanat@hotmail.com>
2025-10-03 12:14:34 +0200

    kommentarer bort gemensam del

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const skickaKnapp = document.getElementById('skickaKnapp')
 const namnInput = document.getElementById ('namnInput')
 const namnHint = document.getElementById('namnHint')
 let giltigtNamn = false
-/* Skicka knapp är disabled så länge alla 3 inte är uppfyllda*/
 const uppdateraForm = () => {
     skickaKnapp.disabled = !(giltigtNamn && giltigTelefon && giltigEpost)
 }
@@ -10,20 +9,17 @@ const uppdateraForm = () => {
 
 namnInput.addEventListener('input', (event) => {
     const namn = event.target.value
-    /*Deklarerar ett regEx med det inbyggda stödet. Mönstret används för att testa inmatning. 
-    Om mönstret passerar testet så returneras true */
+    
     const regExMönster = /^[A-ZÅÄÖa-åäö -]{5,75}$/
     giltigtNamn = regExMönster.test(namn);
 
     if(giltigtNamn){
         namnInput.classList.add('valid')
         namnInput.classList.remove('invalid')
-        //namnHint.classList.add('hidden')
         namnHint.textContent = ''
     } else{
         namnInput.classList.add('invalid')
         namnInput.classList.remove('valid')
-        //namnHint.classList.remove('hidden')
         namnHint.textContent = ': Endast text, 5-75 tecken'
     }
 
@@ -42,12 +38,10 @@ telefonInput.addEventListener('input', (event) => {
     if (giltigTelefon) {
         telefonInput.classList.add('valid')
         telefonInput.classList.remove('invalid')
-        //telefonHint.classList.add('hidden')
         telefonHint.textContent = ''
     } else{
          telefonInput.classList.add('invalid')
         telefonInput.classList.remove('valid')
-        //telefonHint.classList.remove('hidden')
         telefonHint.textContent = ': 7-20 tecken, endast 0-9 samt "+" "-"'
     }
 
@@ -67,12 +61,10 @@ epostInput.addEventListener('input', (event) => {
     if (giltigEpost) {
         epostInput.classList.add('valid')
         epostInput.classList.remove('invalid')
-        //epostHint.classList.add('hidden')
         epostHint.textContent = ''
     } else {
         epostInput.classList.add('invalid')
         epostInput.classList.remove('valid')
-        //epostHint.classList.remove('hidden')
         epostHint.textContent = 'Ex: abc@hotmail.com'
     }
 
